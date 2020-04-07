0228317d2ffaf878d1340ababe9bed76eef5b2b8
Joseph Maclean Arhin <josephmacleanarhin@outlook.com>
2020-04-07 23:59:26 +0000

    refactored init

--- app/__init__.py
+++ app/__init__.py
@@ -4,28 +4,40 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # load dotenv in the base root
-APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')  # refers to application_top
 dotenv_path = os.path.join(APP_ROOT, '.env')
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-from app.clients.views import clients
+
+from .clients.views import clients
 
 
 def create_app():
     """Construct the core application"""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
-    # load db
+
+    # add extensions
+    register_extensions(app)
+
+    # add blueprints
+    register_blueprints(app)
+    return app
+
+
+def register_extensions(app):
+    """Register Flask extensions."""
     db.init_app(app)
-    # load migrations
     migrate.init_app(app, db)
+
     with app.app_context():
-
-        # register blueprints
-        app.register_blueprint(clients)
-
-        # create table for models
         db.create_all()
-        return app
+    return None
+
+
+def register_blueprints(app):
+    """Register Flask blueprints."""
+    app.register_blueprint(clients)
+    return None
